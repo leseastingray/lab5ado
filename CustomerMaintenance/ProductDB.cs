468c18152cc1aa491565323b3bed6e1c3c1aa0eb
Student <Student@1912851-C20251.lanecc.edu>
2019-05-16 15:29:56 -0700

    added Product and ProductDB classes

---
+++ CustomerMaintenance/ProductDB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomerMaintenance
+{
+    public class ProductDB
+    {
+        // get Product object, takes string productCode parameter
+        public static Product GetProduct(string productCode)
+        {
+            SqlConnection connection = MMABooksDB.GetConnection();
+            // SQL select statement. @ goes in front of variables for SQL. * = all
+            string selectStatement
+                = "SELECT * "
+                + "FROM Products "
+                + "WHERE ProductCode = @ProductCode";
+            SqlCommand selectCommand =
+                new SqlCommand(selectStatement, connection);
+            // creates ProductCode parameter to hold value in productCode variable
+            selectCommand.Parameters.AddWithValue("@ProductCode", productCode);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader prodReader =
+                    selectCommand.ExecuteReader(CommandBehavior.SingleRow);
+                //
+                if (prodReader.Read())
+                {
+                    // note all the casting btwn types!
+                    Product product = new Product();
+                    product.ProductCode = prodReader["ProductCode"].ToString();
+                    product.Description = prodReader["Description"].ToString();
+                    product.UnitPrice = (decimal)prodReader["UnitPrice"];
+                    product.OnHandQuantity = (int)prodReader["OnHandQuantity"];
+                    prodReader.Close();
+                    return product;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
